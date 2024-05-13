70a3b5b5728df68ffe07c62809b5960322785f2e
CI User <ci-user@example.com>
2024-05-13 13:03:52 +0000

    Add clsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import {clsx} from 'clsx'
 const title = 'Hello React'
+const classes = clsx('mb-5 bg-blue-500 py-5 text-center text-5xl')
 function App() {
-  return <h1 className="mb-5 py-5 text-5xl text-center bg-blue-500">{title}</h1>
+  return <h1 className={classes}>{title}</h1>
 }
 export default App
